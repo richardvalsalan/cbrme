9af630131b2df00dfbf39888d083116acf4656f1
Richard Valsalan <richard@adodis.com>
2013-11-26 14:49:21 +0530

    Fixing the dropdown issue that is appearing at the moment when an attribute has more option it becomes multiselect the same is fixed now

--- app/code/local/Adodis/Customadvancedsearch/Block/Catalogsearch/Advanced/Form.php
+++ app/code/local/Adodis/Customadvancedsearch/Block/Catalogsearch/Advanced/Form.php
@@ -14,34 +14,21 @@ class Adodis_Customadvancedsearch_Block_Catalogsearch_Advanced_Form extends Mage
 
 	public function getAttributeSelectElement($attribute)
     {
-        // $extra = '';
-        // $options = $attribute->getSource()->getAllOptions(false);
+       $extra = '';
+        $options = $attribute->getSource()->getAllOptions(false);
 
-        // $name = $attribute->getAttributeCode();
+        $name = $attribute->getAttributeCode();
 
-        // // 2 - avoid yes/no selects to be multiselects
-        // if (is_array($options) && count($options)>2) {
-        //     $extra = 'multiple="multiple" size="4"';
-        //     $name.= '[]';
-        // }
-        // else {
-        	
-        // 	//Adding an If condition inorder to make India selected Default
-        //     if ($attribute->getFrontendLabel() != 'Country') {
-        //         array_unshift($options, array('value'=>'', 'label'=>Mage::helper('catalogsearch')->__('All')));
-        //     }
-        // }
+        array_unshift($options, array('value'=>'', 'label'=>Mage::helper('catalogsearch')->__('All')));
 
-        // return $this->_getSelectBlock()
-        //     ->setName($name)
-        //     ->setId($attribute->getAttributeCode())
-        //     ->setTitle($this->getAttributeLabel($attribute))
-        //     ->setExtraParams($extra)
-        //     ->setValue($this->getAttributeValue($attribute))
-        //     ->setOptions($options)
-        //     ->setClass('multiselect')
-        //     ->getHtml();
-
-        return parent::getAttributeSelectElement($attribute);
+        return $this->_getSelectBlock()
+            ->setName($name)
+            ->setId($attribute->getAttributeCode())
+            ->setTitle($this->getAttributeLabel($attribute))
+            //->setExtraParams($extra)
+            ->setValue($this->getAttributeValue($attribute))
+            ->setOptions($options)
+           // ->setClass('multiselect')
+            ->getHtml();
     }
 }
